1f1d50589fbac11fbcab3fd4cf8587b3d5141023
Admin <makansari@gmail.com>
2020-11-10 16:49:32 +0530

    Logs

--- app/src/main/java/com/example/verizonfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/verizonfirstapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.e("mytag","MainActivity Created")
         buttonSubmit.setOnClickListener {
 
           /* var username = editTextUsername.text.toString()
@@ -22,6 +24,9 @@ class MainActivity : AppCompatActivity() {
             var myIntent = Intent(this,SecondActivity::class.java)
             myIntent.putExtra("keyusername",username)
             startActivity(myIntent)*/
+            var myIntent = Intent(this,SecondActivity::class.java)
+
+            startActivity(myIntent)
 
            /* var myIntent = Intent("karthik")
             startActivity(myIntent)*/
@@ -31,10 +36,52 @@ class MainActivity : AppCompatActivity() {
 
           /*  var myIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98585858585"))
             startActivity(myIntent)*/
-            var myIntent = Intent(Intent.ACTION_VIEW,Uri.parse("http://google.com"))
-            startActivity(myIntent)
+            /*var myIntent = Intent(Intent.ACTION_VIEW,Uri.parse("http://google.com"))
+            startActivity(myIntent)*/
 
 
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.w("mytag","MainActivity Started")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("mytag","MainActivity Resumed")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("mytag","MainActivity Paused")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("mytag","MainActivity Stopped")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("mytag","MainActivity Destoryed")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.i("mytag","MainActivity Restarted")
+
     }
 }
--- app/src/main/java/com/example/verizonfirstapp/SecondActivity.kt
+++ app/src/main/java/com/example/verizonfirstapp/SecondActivity.kt
@@ -1,7 +1,9 @@
 package com.example.verizonfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -9,7 +11,58 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+        Log.w("mytag","SecondActivity created")
+
+
 //       var result = intent.extras?.getString("keyusername")
 //        textViewSecondResult.setText(result)
+
+        textViewSecondResult.setOnClickListener {
+            var i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.w("mytag","SecondActivity Started")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("mytag","SecondActivity Resumed")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("mytag","SecondActivity Paused")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("mytag","SecondActivity Stopped")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("mytag","SecondActivity Destoryed")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.i("mytag","SecondActivity Restarted")
+
     }
 }
